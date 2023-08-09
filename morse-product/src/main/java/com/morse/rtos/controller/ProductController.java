747dc676b2d1e253ddb9a50cd9c81ee51efe6f18
kangkang <2010242074@qq.com>
2023-08-09 21:47:48 +0800

    chore: simplify pom

--- morse-product/src/main/java/com/morse/rtos/controller/ProductController.java
+++ morse-product/src/main/java/com/morse/rtos/controller/ProductController.java
@@ -1,17 +1,16 @@
 package com.morse.rtos.controller;
 
 import com.morse.rtos.feign.UserFeign;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resource;
-
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/product")
 public class ProductController {
 
-    @Resource
-    private UserFeign userFeign;
+    private final UserFeign userFeign;
 
     @RequestMapping("/getUserName")
     String getProduct(){
